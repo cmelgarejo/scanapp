bdf0e33422cb789f38c5470d3855fed21575808c
Christian Melgarejo Bresanovich <cmelgarejo.dev@gmail.com>
2017-05-07 22:26:00 -0400

    Reinstantiated App Policy, heroku needs the file.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,3 +1,58 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    scope.where(:id => record.id).exists?
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+
+  def scope
+    Pundit.policy_scope!(user, record.class)
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope
+    end
+  end
+end
+
 # class ApplicationPolicy
 #   attr_reader :user, :record
 #
@@ -7,94 +62,39 @@
 #   end
 #
 #   def index?
-#     false
+#     true
 #   end
 #
 #   def show?
-#     scope.where(:id => record.id).exists?
-#   end
-#
-#   def create?
-#     false
+#     scope.where(id: record.id).exists?
 #   end
 #
 #   def new?
 #     create?
 #   end
 #
-#   def update?
-#     false
+#   def create?
+#     true
 #   end
 #
 #   def edit?
 #     update?
 #   end
 #
-#   def destroy?
-#     false
+#   def update?
+#     true
 #   end
 #
+#   def destroy?
+#     true
+#   end
+#
+#   def destroy_all?
+#     true
+#   end
 #
 #   def scope
 #     Pundit.policy_scope!(user, record.class)
 #   end
-#
-#   class Scope
-#     attr_reader :user, :scope
-#
-#     def initialize(user, scope)
-#       @user = user
-#       @scope = scope
-#     end
-#
-#     def resolve
-#       scope
-#     end
-#   end
 # end
-#
-# # class ApplicationPolicy
-# #   attr_reader :user, :record
-# #
-# #   def initialize(user, record)
-# #     @user = user
-# #     @record = record
-# #   end
-# #
-# #   def index?
-# #     true
-# #   end
-# #
-# #   def show?
-# #     scope.where(id: record.id).exists?
-# #   end
-# #
-# #   def new?
-# #     create?
-# #   end
-# #
-# #   def create?
-# #     true
-# #   end
-# #
-# #   def edit?
-# #     update?
-# #   end
-# #
-# #   def update?
-# #     true
-# #   end
-# #
-# #   def destroy?
-# #     true
-# #   end
-# #
-# #   def destroy_all?
-# #     true
-# #   end
-# #
-# #   def scope
-# #     Pundit.policy_scope!(user, record.class)
-# #   end
-# # end
-#
+
