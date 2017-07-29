5bcf9a1ffafafb907945de65c30995483a4aa229
Christian Melgarejo Bresanovich <cmelgarejo.dev@gmail.com>
2017-07-29 12:06:49 -0400

    Added more fields for referred parents and childs on the API

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -38,7 +38,7 @@ class Api::V1::ItemsController < ApiController
   end
 
   def parents_except_fields
-    (template_except_fields << %w(country state city extra_properties)).flatten
+    (generic_except_fields << %w(is_template is_root company_id item_id country state city extra_properties)).flatten
   end
 
   def categories_except_fields
